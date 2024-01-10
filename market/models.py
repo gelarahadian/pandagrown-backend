2fa6fc7fd5c87db9fffbba99481316e016024f70
Zaini Azhar <zainiazhar2003@gmail.com>
2024-01-11 00:12:17 +0700

    add unit on market

--- market/models.py
+++ market/models.py
@@ -7,7 +7,16 @@ class Market(models.Model):
     size = models.CharField(max_length=128)
     stock = models.FloatField(default=0)
     benefit = models.IntegerField(default=0)
-    benefit_type = models.CharField(blank=True, null=True)
+    benefit_type_choices = (
+        (0, 'reduce'),
+        (1, 'increase')
+    )
+    benefit_type = models.SmallIntegerField(choices=benefit_type_choices, default=0)
+    unit_choices = (
+        (0, 'day'),
+        (1, 'percentage')
+    )
+    unit = models.SmallIntegerField(choices=unit_choices, default=0)
     cover_img = models.ImageField(upload_to='market/cover/', null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- market/serializers.py
+++ market/serializers.py
@@ -8,9 +8,8 @@ User = get_user_model()
 
 class MarketSerializer(serializers.ModelSerializer):
     cover_img = serializers.ImageField(required = False)
-    # referer_avatar = serializers.ImageField(source='userprofile.referer__avatar', read_only=True)  # Change this line to ImageField
     class Meta:
         model = Market
-        fields = ['id', 'cover_img', 'name', 'buy_price', 'size', 'stock', 'benefit', 'benefit_type']
+        fields = ['id', 'cover_img', 'name', 'unit', 'buy_price', 'size', 'stock', 'benefit', 'benefit_type']
     def update(self, instance, validated_data):
         return super().update(instance, validated_data)
